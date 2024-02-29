9a4307cc7a730bfa2b6d5993386d0674dd4cdfc3
SangeethaAnbalagan <144044263+SangeethaAnbalagan@users.noreply.github.com>
2024-02-29 14:51:37 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,63 @@
+
+
+import React, { Component } from "react";
+import {
+	BrowserRouter as Router,
+	Routes,
+	Route,
+	Link,
+} from "react-router-dom";
+
+import Home from "./Component/Home";
+import About from "./Component/About";
+import Contact from "./Component/Contact";
+
+class App extends Component {
+	render() {
+		return (
+			<>
+			
+			<Router>
+				<div className="App">
+					<ul className="App-header">
+						<li>
+							<Link to="/">Home</Link>
+						</li>
+						<li>
+							<Link to="/about">
+								About Us
+							</Link>
+						</li>
+						<li>
+							<Link to="/contact">
+								Contact Us
+							</Link>
+						</li>
+					</ul>
+					<Routes>
+						<Route
+							exact
+							path="/"
+							element={<Home/>}
+						></Route>
+						<Route
+							exact
+							path="/about"
+							element={<About/>}
+						></Route>
+						<Route
+							exact
+							path="/contact"
+							element={<Contact/>}
+						></Route>
+					</Routes>
+				</div>
+			</Router>
+			</>
+		);
+	}
+}
+
+export default App;
+
+
